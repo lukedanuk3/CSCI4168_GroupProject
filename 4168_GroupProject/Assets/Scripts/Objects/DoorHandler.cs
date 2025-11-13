feaf2f9e3665566c999e5ce0fc98d5300942515b
Adrian Lloyd <adrian-lloyd@outlook.com>
2025-11-13 15:10:33 -0400

    Add DoorHandler

---
+++ 4168_GroupProject/Assets/Scripts/Objects/DoorHandler.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class DoorHandler : MonoBehaviour
+{
+    
+    void Interact(){
+        //open or close door
+    }
+    
+}
--- 4168_GroupProject/Assets/Scripts/Objects/InteractionHandler.cs
+++ 4168_GroupProject/Assets/Scripts/Objects/InteractionHandler.cs
@@ -6,16 +6,18 @@ public class InteractionHandler : MonoBehaviour
     //Constant types for objects -- each one determines interaction behaviour;
     //Allows for script modularity (same script for tools and other interactables)
     const string TOOL = "TOOL";
-    const string HEALTH_BOOST = "HEALTHBOOST";
-    //etc, etc...
+    const string DOOR = "DOOR";
+    const string BUTTON = "BUTTON";
 
     public string objectType;
 
     public void Interact(GameObject player){
         if (objectType == TOOL){
             player.GetComponent<PlayerControl>().EquipTool(gameObject);
-        }else if (objectType == HEALTH_BOOST){
-            //boost health or something idk lol this is a placeholder example
+        }else if (objectType == DOOR){
+            gameObject.GetComponent<DoorHandler>().Interact();
+        }else if (objectType == BUTTON){
+            //TODO: buttons?
         }
     }
 }
