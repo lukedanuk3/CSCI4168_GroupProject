79317d2adffee13301db4d1d2b043f13c56740cd
lukedanuk3 <160971277+lukedanuk3@users.noreply.github.com>
2025-11-23 14:24:56 -0400

    Imported Prefab assets from Adrian's Branch
    Imported some of Adrian's Prefab assets from his branch, and am currently working on door handling

--- CSCI4168_GroupProject-main/4168_GroupProject/Assets/Scripts/Objects/DoorHandler.cs
+++ CSCI4168_GroupProject-main/4168_GroupProject/Assets/Scripts/Objects/DoorHandler.cs
@@ -1,12 +1,35 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorHandler : MonoBehaviour
 {
-    
+
+    public GameObject doorOpenInstructions;
     public void Interact(){
-        Debug.Log("Interacted with door");
+            if(Input.GetKeyDown(KeyCode.E)){
+                Interact();
+                gameObject.transform.Rotate(0,90,0, Space.World);        
+                Debug.Log("Interacted with door");
+            }
         //open or close door...
+    }
+
+    private void OnTriggerStay(Collider other){
+        if(other.tag == "Door"){
+            doorOpenInstructions.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.E)){
+                Interact();
+                other.transform.Rotate(0,90,0, Space.World);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        if(other.tag == "Door"){
+            doorOpenInstructions.SetActive(false);
+            other.transform.Rotate(0,-90,0, Space.World);
+        }
     }
     
 }
