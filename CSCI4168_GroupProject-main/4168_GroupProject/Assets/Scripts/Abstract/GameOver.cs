5a071ac5b67534f1845ace66f61819f88bd5f906
lukedanuk3 <160971277+lukedanuk3@users.noreply.github.com>
2025-11-30 20:15:06 -0400

    Added Danielle's Game over script

--- CSCI4168_GroupProject-main/4168_GroupProject/Assets/Scripts/Abstract/GameOver.cs
+++ CSCI4168_GroupProject-main/4168_GroupProject/Assets/Scripts/Abstract/GameOver.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum buttonOptions
     {
-        
+        quit,
+        reload
     }
 
-    // Update is called once per frame
-    void Update()
+    public buttonOptions choice;
+
+    public void OnButtonClick()
     {
-        
+        if (choice == buttonOptions.quit)
+        {
+            // note: this code will be ignored in unity's editor mode, works in standalone game
+            Debug.Log("This will quit the game.");
+            Application.Quit();
+        }
+        if (choice == buttonOptions.reload)
+        {
+            //reload the current scene
+            Debug.Log("This will reload the current level.");
+            string currentScene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentScene);
+        }
     }
 }
